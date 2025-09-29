a3700918f2929035d277bf3b0a6dd18e60824047
AdityaDudhal <a.dudhal@maxxton.com>
2025-09-29 11:22:20 +0530

    Enhance App.jsx and main.jsx with additional comments for better understanding of JSX and React.createElement usage

--- 01vitereactproject/src/App.jsx
+++ 01vitereactproject/src/App.jsx
@@ -1,8 +1,21 @@
 import Chai from "./Chai";
 
 function App() {
+  const userName = "Aditya";
+
   return (
-    <Chai></Chai>
+    <>
+      {/* Rendering the Chai component */}
+      <Chai></Chai>
+
+      {/* Explanation about how JSX treats expressions */}
+      {/* Anything inside {} is treated as JavaScript expression */}
+      {/* JSX allows embedding expressions inside {} */}
+      {/* Example: 2 + 2, userName, Math.random(), function calls etc. */}
+      {/* We do not write full JavaScript here but write the final outcome of the expression */}
+      {/* This is not allowed{if(true) username=""}*/}
+      <h1>Welcome to React, {userName}!</h1>
+    </>
   )
 }
 
--- 01vitereactproject/src/main.jsx
+++ 01vitereactproject/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
 
 // This is the main entry point of the React application. It uses ReactDOM to render the App component into the root DOM node.'
 // StrictMode is a tool for highlighting potential problems in an application.
@@ -20,10 +22,24 @@ const element = (
   </a>
 )
 
+const anotherUser = "Another Aditya"
+
+//  This is how React.createElement works
+//  React.createElement(type, [props], [...children])
+//  type: This is the type of the element. It can be a string (for HTML elements) or a React component.
+//  props: This is an object containing the properties of the element. It can be null.
+//  children: These are the child elements of the element. It can be a single child or an array of children.
+//  This is same as we created our own react element in customreact.js
+const reactElement = React.createElement(
+'a',
+{href: 'https://www.google.com', target: '_blank'},
+anotherUser
+);
+
 createRoot(document.getElementById('root')).render(
   //MyApp()
   //element
-
+  //reactElement
 
   /*This App component will be converted to object like below by babel transpiler
   {
@@ -41,6 +57,7 @@ createRoot(document.getElementById('root')).render(
   The root element is a div with id 'root'
   The root element is selected using document.getElementById('root')
   The root element is passed to ReactDOM.render() method as second argument
-  The react element is passed to ReactDOM.render() method as first argument */
+  The react element is passed to ReactDOM.render() method as first argument */  
+
   <App></App>
 )
