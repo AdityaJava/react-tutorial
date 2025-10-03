7add3deb125b8bdff13189faa98fe8a93c216db9
AdityaDudhal <a.dudhal@maxxton.com>
2025-10-03 10:50:55 +0530

    Refactor code structure for improved readability and maintainability

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './App.css' 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-6xl font-bold text-yellow-600 bg-red-500 p-6">Tailwind test</h1>
-    </>
+    <div>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+    </div>
   )
 }
 
