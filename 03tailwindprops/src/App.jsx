a84d0d791b60bff8f1e57d4c625177477362168d
AdityaDudhal <a.dudhal@maxxton.com>
2025-10-08 09:57:47 +0530

    Refactor UserCard component to accept individual props instead of an object for improved clarity and usability

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -13,8 +13,10 @@ function App() {
   return (
     <div>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-      <UserCard  obj = {backendDev}></UserCard>
-      <UserCard obj = {frontendDev}></UserCard>
+      <br></br>
+      <UserCard  name="krishna" role="FullStack Java Spring boot" ></UserCard>
+      <br></br>
+      <UserCard name="RadhaRani" role="Backend Dev" text="Hire Me" ></UserCard>
     </div>
   )
 }
--- 03tailwindprops/src/card.jsx
+++ 03tailwindprops/src/card.jsx
@@ -1,8 +1,7 @@
 
-function UserCard({obj}){
+function UserCard({name = 'RadhaRani', role = 'FullStack Java', text = 'Visit me'}){
   //console.log('props',props.obj)
     // Destructure the values from obj and provide defaults
-    const { name = 'RadhaRani', role = 'FullStack Java', text = 'Visit me' } = obj;
 
   return(
     <div className="max-w-xs mx-auto">
@@ -18,7 +17,7 @@ function UserCard({obj}){
         <div>
           <h2 className="text-sm font-semibold text-slate-900">{name}</h2>
           <p className="text-xs text-slate-500">{role}</p>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{text}</button>
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{text}</button>
           </div>
       </div>
     </div>
