fcb3669177701e25f10c6993b5741cd8d45952c2
AdityaDudhal <a.dudhal@maxxton.com>
2025-10-08 09:26:52 +0530

    Refactor UserCard component to use 'props' instead of 'aditya' for better clarity and adherence to best practices

--- 03tailwindprops/src/card.jsx
+++ 03tailwindprops/src/card.jsx
@@ -1,6 +1,6 @@
 
-function UserCard(aditya){
-  console.log('props',aditya)
+function UserCard(props){
+  console.log('props',props)
   return(
     <div className="max-w-xs mx-auto">
     <div className="bg-white rounded-xl shadow-md overflow-hidden ring-1 ring-slate-200">
