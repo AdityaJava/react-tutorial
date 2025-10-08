9c83959b53dd3d66a22594c9194153792d481f4e
AdityaDudhal <a.dudhal@maxxton.com>
2025-10-08 09:52:30 +0530

    Refactor UserCard component to destructure props for improved readability and maintainability

--- 03tailwindprops/src/card.jsx
+++ 03tailwindprops/src/card.jsx
@@ -1,6 +1,9 @@
 
-function UserCard(props){
-  console.log('props',props.obj)
+function UserCard({obj}){
+  //console.log('props',props.obj)
+    // Destructure the values from obj and provide defaults
+    const { name = 'RadhaRani', role = 'FullStack Java', text = 'Visit me' } = obj;
+
   return(
     <div className="max-w-xs mx-auto">
     <div className="bg-white rounded-xl shadow-md overflow-hidden ring-1 ring-slate-200">
@@ -13,9 +16,9 @@ function UserCard(props){
           loading="lazy"
         />
         <div>
-          <h2 className="text-sm font-semibold text-slate-900">{props.obj.name}</h2>
-          <p className="text-xs text-slate-500">{props.obj.role}</p>
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{props.obj.text || "Visit me"}</button>
+          <h2 className="text-sm font-semibold text-slate-900">{name}</h2>
+          <p className="text-xs text-slate-500">{role}</p>
+          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{text}</button>
           </div>
       </div>
     </div>
