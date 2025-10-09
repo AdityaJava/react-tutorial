07f8def356692eee100a3933f11c1f17184f9416
AdityaDudhal <a.dudhal@maxxton.com>
2025-10-09 09:57:48 +0530

    fix: increase duration of background color transition in App component

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [ color, setColor ] = useState( "olive" )
 
   return (
-    <div className='w-full h-screen duration-200' style={ { backgroundColor: color } }>
+    <div className='w-full h-screen duration-2000' style={ { backgroundColor: color } }>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 '>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white'>
           <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
