9b663dcc735cfe081e37d21dffbdb5dafc04a0dd
aditya-cresults <adudhalcresults@gmail.com>
2025-11-21 18:49:18 +0530

    feat: implement Todo functionality with add and remove features using Redux Toolkit

---
+++ 08reduxtoolkittodo/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+function Todos() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={dispatch(removeTodo(todo.id))}></button>
+        </li>;
+      })}
+    </div>
+  );
+}
