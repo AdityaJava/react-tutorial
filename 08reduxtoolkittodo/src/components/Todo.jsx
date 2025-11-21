1888c8b7d3d81c2c3711a7c743092346aedd469c
aditya-cresults <adudhalcresults@gmail.com>
2025-11-21 19:10:42 +0530

    feat: update Todos component layout and add debugger for development

--- 08reduxtoolkittodo/src/components/Todo.jsx
+++ 08reduxtoolkittodo/src/components/Todo.jsx
@@ -2,11 +2,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
+  debugger;
   const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className="mt-12" width="400px">
       {todos.map((todo) => {
         <li key={todo.id}>
           {todo.text}
