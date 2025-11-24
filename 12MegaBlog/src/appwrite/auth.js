5628e082439da1fa1d18497df86be035a889b394
aditya-cresults <adudhalcresults@gmail.com>
2025-11-24 13:49:54 +0530

    feat: implement AuthService with Appwrite integration and restore conf.js

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,36 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // Call another method
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
