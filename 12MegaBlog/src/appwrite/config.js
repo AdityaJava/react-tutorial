27df6eb6b0b6484beab5aac6ac741be6e617508f
aditya-cresults <adudhalcresults@gmail.com>
2025-11-24 15:54:12 +0530

    feat: implement Service class for Appwrite integration with createPost method

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,38 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appWriteUrl)
+      .setProject(conf.appWriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Error in createPosr", error);
+    }
+  }
+}
+
+const service = new Service();
+
+export default service;
