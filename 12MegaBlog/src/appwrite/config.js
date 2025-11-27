610a50cfe770f4ecff0c6e5393ea5e73fa4ca734
aditya-cresults <adudhalcresults@gmail.com>
2025-11-27 11:27:54 +0530

    feat: add CRUD operations and file handling methods to Service class for Appwrite integration

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -31,6 +31,89 @@ export class Service {
       console.log("Error in createPosr", error);
     }
   }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite erro: updatePost", error)
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(conf.appWriteDatabaseId, conf.appWriteCollectionId, slug);
+      return true;
+    } catch (error) {
+      console.log("Error in deletePost", error);
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        slug
+      )
+    } catch (error) {
+      console.log("Error in getPost", error);
+
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appWriteDatabaseId,
+        conf.appWriteCollectionId,
+        queries
+      )
+    } catch (error) {
+      console.log("Error in getPosts", error);
+    }
+  }
+
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appWriteBucketId,
+        ID.unique(),
+        file
+      )
+    } catch (error) {
+      console.log("Error in uploadFile", error);
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appWriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Error in deleteFile", error);
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(
+      conf.appWriteBucketId,
+      fileId
+    )
+  }
+
 }
 
 const service = new Service();
